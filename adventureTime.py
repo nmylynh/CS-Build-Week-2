e6eef115644dfa9963575e15261c5384c72a91fb
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2020-01-14 01:57:52 -0800

    Update adventureTime.py

--- adventureTime.py
+++ adventureTime.py
@@ -1,7 +1,7 @@
 from player import *
 
 adventure = adv()
-adventure.accumulate=True   # set pick up items to false
+adventure.accumulate=True   # if you want to pick up items, set to True
 adventure.import_text_map=True
 adventure.want_to_mine=True # if you want to mine even though you had already mined, set to True
 adventure.create_starting_map()
