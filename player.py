7aabf2dd49b7c2c3f0fa275d85f0f8c8b5640769
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2020-01-14 02:55:02 -0800

    Update player.py

--- player.py
+++ player.py
@@ -442,35 +442,6 @@ class adv:
                     print(f"Your current gold: {player['gold']}")
                 print('Back to Looting')
         
-
-    def get_coins(self):
-      # Want this to do 3 things:
-      # Function to go to the wishing well and examine
-      # Function to go to where the wishing well says
-      # Function to mine coin at specified location
-      # Could include if clause to go transmog coins
-
-      coins = 0
-      # variable for proof?
-      while coins < 1000:
-        # Go to wishing well
-        print('Going to the Wishing Well.')
-        self.wishing_well()
-        # Examine well
-        self.action('examine')
-        # Go to where it says
-        self.go_to_room('hinted location/room')
-        # Mine Coin
-        print('Getting proof...')
-        response = request.post(f'https://lambda-treasure-hunt.herokuapp.com/api/bc/last_proof/', headers=self.headers)
-        new_proof = proof_of_work(data.get('proof'), data.get('difficulty'))
-        time.sleep(self.wait)
-        # Need to send new_proof in the mine request json
-        response = request.post(f'https://lambda-treasure-hunt.herokuapp.com/api/bc/mine/', headers=self.headers, json={"proof":''})
-        print('You got a coin!')
-        coins += 1
-        time.sleep(self.wait)
-
     def hint_to_ld8(self):
         "converts hint in well to room number"
         self.action('examine','well')
